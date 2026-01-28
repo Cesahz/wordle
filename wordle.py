0d720ec62e40f87768d1d191b4fbddb91aa1a849
Cesar Espinola <espinolac999@gmail.com>
2026-01-28 14:34:38 -0300

    agrego las funciones principales (para solicitar palabras, resaltar las letras correctas, controlar los intentos)

--- wordle.py
+++ wordle.py
@@ -15,3 +15,48 @@ limite_letra = 6
 intentos = 6
 
 jugando = True
+
+#------2-Definir funciones para el juego
+
+#Pedimos la palabra y verificamos que solo tenga 6 letras, tambien restamos un intento por cada palabra ingresada
+def Solicitar_palabra():
+    ingresar_palabra = input('Escribe una palabra: ')
+    while len(ingresar_palabra) != len(palabra_elegida):
+        print('La palabra tiene que tener 6 letras, vuelve a escribir otra palabra: ')
+        ingresar_palabra = input('Escribe otra palabra que tenga 6 letras: ')
+    return ingresar_palabra
+
+#Resaltar las letras que coinciden
+def Resaltar_letra(palabra_puesta,palabra_clave):
+    letras = []
+    for i in range(limite_letra):
+        letra_p = palabra_puesta[i]
+        letra_c = palabra_clave[i]
+        if letra_p == letra_c:
+            letras.append(f'[{letra_p}]')
+        elif letra_p in palabra_clave:
+            letras.append(f'({letra_p})')
+        else:
+            letras.append(f'{letra_p}')
+    return letras
+
+#Creo una funcion para controlar los intentos
+def Controlar_intentos(intento_actual):
+    if intento_actual < 1:
+        sigo_jugando = False
+        return sigo_jugando
+    else:
+        sigo_jugando = True
+        return sigo_jugando
+
+#Avisar si ganamos o perdimos
+def Resultado_juego():
+    if not jugando:
+        print('Perdiste el juego. \n ')
+    elif palabra_elegida == palabra_ingresada:
+        print(f'Ganaste el juego, te tomo solo {6 - intentos} intentos \n')
+        sigo_jugando = False
+        return sigo_jugando
+    else:
+        sigo_jugando = True
+        return sigo_jugando
